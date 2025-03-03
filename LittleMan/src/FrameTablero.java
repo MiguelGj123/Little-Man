b115e7845cc6055c947124fb2cd458231ea3801a
Alejandro <alejandrommoreno2003@gmail.com>
2025-03-03 21:18:21 +0100

    añadido funciones basicas de FrameTablero y funciones basicas de Controller

--- LittleMan/src/FrameTablero.java
+++ LittleMan/src/FrameTablero.java
@@ -1,17 +1,15 @@
-import java.awt.EventQueue;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.*;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
-public class FrameTablero extends JFrame {
+public class FrameTablero extends JFrame implements Observer {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private Escenario escenario;
 
-	/**
-	 * Launch the application.
-	 */
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -29,12 +27,53 @@ public class FrameTablero extends JFrame {
 	 * Create the frame.
 	 */
 	public FrameTablero() {
+		escenario = Escenario.getEscenario();
+		escenario.addObserver(this);
+		
+		setTitle("Bomberman");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 800, 600);
+		setResizable(false);
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout());
+		
+		addKeyListener(new Controller());
+	}
+	
+	@Override
+	public void update(Observable o, Object arg) {
+		repaint();
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
 	}
 
 }
+
+class Controller implements KeyListener {
+	private Escenario escenario;
+	
+	public Controller() {
+		this.escenario = Escenario.getEscenario();
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int keyCode = e.getKeyCode();
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent e) {}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {}
+	
+}
+
+
+
