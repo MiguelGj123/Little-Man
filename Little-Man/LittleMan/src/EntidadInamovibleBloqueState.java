f4f0cf09a1415ea1d761dd290ff526a2322e4520
AdrianRamirez05 <adrianramirezgomez05@gmail.com>
2025-03-28 20:08:43 +0100

    Delete Little-Man/LittleMan/src directory

--- Little-Man/LittleMan/src/EntidadInamovibleBloqueState.java
+++
@@ -1,9 +0,0 @@
-
-public interface EntidadInamovibleBloqueState {
-	public String getTipo();
-	public int getCodigoBloque();
-	public boolean romperBloque(EntidadInamovibleBloque pBloque);
-	public boolean getJugadorChocaContraCelda();
-	public boolean getPuedeSerExplotado();
-	
-}
