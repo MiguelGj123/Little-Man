a4c9eaea908edb28ab6ed786840359cb20732c0b
MiguelGj123 <miguelgarciajimenez1998@gmail.com>
2025-03-29 21:13:49 +0100

    Se ha cambiado el main para que se cree el Frame Inicio
    Para ver el anterior Frame (el de tablero) comentar uno y descomentar el otro.

--- Little-Man/LittleMan/src/Main.java
+++ Little-Man/LittleMan/src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args)
     {
 
+        //FrameInicio frameInicio = new FrameInicio();
+        //frameInicio.setVisible(true);
+        
         FrameTablero frame = new FrameTablero("NEGRO");
         frame.setVisible(true);
 
