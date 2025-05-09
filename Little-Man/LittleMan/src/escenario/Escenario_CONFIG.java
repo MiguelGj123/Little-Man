9d066dc09267e6f283d85396011e1acc76b3d4b4
MiguelGj123 <miguelgarciajimenez1998@gmail.com>
2025-05-09 20:15:11 +0200

    Delete Little-Man/LittleMan/src directory

--- Little-Man/LittleMan/src/escenario/Escenario_CONFIG.java
+++
@@ -1,6 +0,0 @@
-package escenario;
-
-public class Escenario_CONFIG {
-	public final int fil = 11;
-	public final int col = 17;
-}
--- Little-Man/LittleMan/src/frameTablero/ActualizableImagen.java
+++
@@ -1,5 +0,0 @@
-package frameTablero;
-
-public interface ActualizableImagen {
-    public void actualizarImagen(int codigo, int col, int fil);
-}
